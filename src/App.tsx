f8cbd84b6409e94c32e3eb9febe09e38f3299654
Neto Rissi <netorissi@hotmail.com>
2020-07-12 20:29:41 -0300

    change import hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/signIn';
 // import SignUp from './pages/signUp';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
